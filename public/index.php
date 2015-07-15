aa18b6441e8be1da2b909a5bd3de5c231be48eb2
Paula C Schuler <paulasatx@gmail.com>
2015-07-15 12:33:21 -0500

    photo images from original site uploaded as assets along with placeholder content.

---
+++ public/index.php
@@ -0,0 +1,88 @@
+<?php 
+// include '/data.php';
+ ?>
+
+<!DOCTYPE html>
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap-theme.min.css">
+    <link rel="stylesheet" type="text/css" href="/css/normalize.css">
+    <link rel="stylesheet" type="text/css" href="/css/style.css">
+    <link rel="stylesheet" type="text/css" href="/css/grid.css">
+
+    <script src="https://use.fonticons.com/6f4f57ed.js"></script>
+
+  <title>Esther</title>
+
+</head>
+<body>
+    <div class="container">
+
+        <!-- Navigation Bar Top -->
+        <nav class="row">
+            <ul class="column">
+           
+                <li><a href="#"></a>Home</li>
+                <li><a href="#"></a>About</li>
+                <li><a href="#"></a>Books</li>
+                <li><a href="#"></a>Coaching</li>
+            </ul>
+        </nav>
+
+        <div class="row hero-home hero-large">
+            <div class="offset-one-third col-1-3 hero-box-large">
+                <h1>Esther Hershenhorn</h1>
+            </div>
+        </div>
+
+        <!-- Begin Summaries -->
+        <div class="row">
+            <div class="col-1-3 summary">
+                <a class="offset-one-third" href=""><i class="icon icon-5x icon-star"></i></a>
+                <h3 class="offset-one-third">About</h3>
+                <p>Lucky me! I spend my days doing what I love and loving what I do! I write 
+                    picture books, board books, middle grade fiction. I teach adults writing for
+                    children and coach writers of all ages to help them tell their stories. 
+                </p>
+                <a href="">More</a>
+
+            </div>  
+            <div class="col-1-3 summary">
+                <a class="offset-one-third" href=""><i class="icon icon-5x icon-book"></i></a>
+                <h3 class="offset-one-third">Books</h3>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                consequat.</p>
+                <a href="">More</a>
+            </div>
+            <div class="col-1-3 summary">
+                <a class="offset-one-third" href=""><i class="icon icon-5x icon-exchange"></i></a>
+                <h3 class="offset-one-third">Coaching</h3>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                consequat.</p>
+                <a href="">More</a>
+            </div>  
+            </hr>  
+        </div>
+
+        <footer>
+            <div class="byline"> &copy; 2015 Esther Hershenhorn 
+            </div>
+        </footer>
+
+
+
+
+
+
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+</div>
+</body>
+</html>
