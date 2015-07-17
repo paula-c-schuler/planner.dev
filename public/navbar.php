4ad4c6978c5f817a1bf8ddd811347fc5f4884c37
Paula C Schuler <paulasatx@gmail.com>
2015-07-16 20:13:32 -0500

    Navbar has its own file. Styling improved. Links working.

---
+++ public/navbar.php
@@ -0,0 +1,10 @@
+<!-- WANT TODO: ADD PNG OF ESTHER'S NAME AS IMAGE ICON THAT STAYS AT THE VERY TOP ON SMALL SCREENS -->
+
+<nav class="row">
+    <ul class="">
+        <li><a href="index.php">Home</a></li>
+        <li><a href="about.php">About</a></li>
+        <li><a href="books.php">Books</a></li>
+        <li><a href="coaching.php">Coaching</a></li>
+    </ul>
+</nav>
