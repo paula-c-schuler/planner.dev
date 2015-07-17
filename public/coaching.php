c10d5ff0e6def590299fce71270568a82566033d
Paula C Schuler <paulasatx@gmail.com>
2015-07-16 20:11:04 -0500

    Coaching page layed out for initial design.

---
+++ public/coaching.php
@@ -0,0 +1,105 @@
+<?php 
+// include '/data.php';
+ ?>
+
+<!DOCTYPE html>
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap-theme.min.css">
+    <link rel="stylesheet" type="text/css" href="/css/normalize.css">
+    <link rel="stylesheet" type="text/css" href="/css/style.css">
+    <link rel="stylesheet" type="text/css" href="/css/grid.css">
+
+    <script src="https://use.fonticons.com/6f4f57ed.js"></script>
+
+  <title>Esther</title>
+
+</head>
+<body>
+    <!-- Navigation Bar Top -->
+        <?php include 'navbar.php'; ?>
+
+    <!-- Begin Container  -->
+    <div class="container">
+
+        <!-- Hero -->
+        <div class="row hero-home hero-large">
+            <div class="offset-one-third col-1-3 hero-box-large">
+                <h1 class="hero-text">quote on coaching</h1>
+            </div>
+        </div>
+
+        <!-- Begin Summaries -->
+        <div class="row">
+            <div class="col-1-3 summary">
+                <a class="offset-one-third" href="about.php"><i class="icon icon-5x icon-exchange"></i></a>
+                <h3 class="offset-one-sixth">The Experience</h3>
+                <p>
+                    Working with writers wherever they are in the writing and publishing process, I help them
+                    discover, craft and market their stories. Careful manuscript readings and thoughtfully-worded comments reflect my teaching and publishing
+                    experience. 
+                </p>
+                <p>
+                    Presentations occur in person, at a convenient Chicago location, or via a phone consultation. 
+                    These one-on-one meetings offer invaluable insights to a writer's story spark, motivation 
+                    and years on task in his writer’s journey. They also allow allow me the necessary information 
+                    to best accommodate the writer’s story and the story he’s telling.
+                </p>
+                <p>
+                    I aim specific criticisms of a specific manuscript towards universal tenets of writing for 
+                    children in today's markets. 
+                </p>
+            </div>  
+
+            <div class="col-1-3 summary">
+                <a class="offset-one-third" href=""><i class="icon icon-5x icon-usd"></i></a>
+                <h3 class="offset-one-sixth">Fees and Hours</h3>
+                <p>
+                    Rates reflect the hours necessary to thoughtfully read and 
+                    consider your manuscript, then prepare and present a thoughtful, 
+                    instructive analysis that enables you to move forward successfully.
+                </p>
+                <p>
+                    I also now offer Office Hours, personal consultations in person or via the telephone, 
+                    to discuss specific manuscript problems, writing issues, marketing possibilities and career issues.
+                </p>
+                <p>
+                    I often work with Writer's Groups, too, facilitating sessions and/or teaching from the members' manuscripts. 
+                    Fees for such services are negotiated separately.
+                </p>
+            </div>
+
+            <div class="col-1-3 summary">
+                <a class="offset-one-third" href=""><i class="icon icon-5x icon-book"></i></a>
+                <h3 class="offset-one-fourth">Manuscript</h3>
+                <p>
+                    All manuscripts are to be submitted as hard copy, typed, single-sided and 
+                    double-spaced in a 12-point font. 
+                </p>
+                    Please include the manuscript, payment, an  8 ½ x 11 SASE, a 25-word  
+                    summary of the plot and a list of manuscript concerns. 
+                </p>
+                <p>
+                    Turn-around time is usually 4 to 6 weeks.
+                </p>
+            </div>  
+            </hr>  
+        </div>
+
+        <!-- Contact Button -->
+        <?php include 'contact-button.html'; ?>
+
+    </div>
+    
+    <!-- Footer -->
+    <?php include 'footer.php'; ?>
+
+    <script type="text/javascript" src="/js/jquery-2.1.4.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+
+</body>
+</html>
