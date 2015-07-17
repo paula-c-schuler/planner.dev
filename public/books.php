e7536c20b4eaf5a9cd81a8a3a48e1294dffdf2ac
Paula C Schuler <paulasatx@gmail.com>
2015-07-16 20:10:42 -0500

    Books page layed out for initial design.

---
+++ public/books.php
@@ -0,0 +1,370 @@
+<?php 
+// include '/data.php';
+ ?>
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap-theme.min.css">
+    <link rel="stylesheet" type="text/css" href="/css/normalize.css">
+    <link rel="stylesheet" type="text/css" href="/css/style.css">
+    <link rel="stylesheet" type="text/css" href="/css/grid.css">
+
+    <script src="https://use.fonticons.com/6f4f57ed.js"></script>
+
+	<title>Esther About</title>
+</head>
+<body>
+    <!-- Navigation Bar Top -->
+    <?php include 'navbar.php'; ?>
+    
+    <!-- Begin Container -->
+    <div class="container">
+
+        <!-- Hero -->
+        <div class="row hero-home hero-large">
+            <div class="offset-one-third col-1-3 hero-box-large">
+                <h1 class="hero-text">quote about books being awesome.</h1>
+            </div>
+        </div>
+
+        <!-- Begin Books List -->
+<!-- Texting -->
+            <div class="row book">
+                <img class="col-md-2 offset-one-fifth top-buffer " src="/img/book-texting.jpg">
+                
+                <div class="col-md-6">
+                    <h2><em>Txting Mama Txting Baby</em>
+                    </h2>
+                    <p>
+                        By Esther Hershenhorn<br>
+                        Sleeping Bear Press, 2012<br>
+                        ISBN: 978-1585368877
+                    </p>
+                    <br>
+                    <p>
+                        It’s a Techy-Techy World 4 2day’s Babies. Smiley Face
+                        <br>
+                        Thanks to their parents’ smart phones, tablets and computers, babies 
+                        everywhere are tapping keyboards and finger-swiping screens, 
+                        honing digital skills while living their baby lives.
+                        <br>
+                        "Txting Mama Txting Baby" playfully brings this newest of worlds to the 
+                        sturdy pages of the ultimate hand-held device: the baby board book! 
+                    </p>
+                    <a target="_blank" href="http://www.amazon.com/Txtng-Mama-Baby-Esther-Hershenhorn/dp/1585368873/ref=sr_1_3?s=books&ie=UTF8&qid=1437060651&sr=1-3&keywords=esther+hershenhorn">Buy on Amazon</a>
+                </div>
+
+
+                </div>
+    			<br>
+    			<br>
+<!-- nice on small screens up to 990, add class for large screens -->
+                <div class="pretty-divider col-md-12">
+                </div>
+            </div>
+			
+<!-- Friday Anthology -->
+            <div class="row book">
+                <img class="col-md-2 offset-one-fifth top-buffer " src="/img/book-poetry-friday.jpg">
+                
+                <div class="col-md-6">
+                    <h2><em>The Poetry Friday Anthology: Poems for the School Year with Connections to the Common Core</em>
+                    </h2>
+                    <p>
+                        Compiled by Sylvia Vardell and Janet Wong <br>
+                        Pomelo Books, 2012  <br>
+                        ISBN:  978-1-937057-68-8
+                    </p>
+                    <br>
+                    <p>
+                        The Poetry Friday Anthology offers a set of 36 poems for each grade level K-5,
+                        a poem-a-week for the 9 months of the typical school year! 
+                        <br>
+                        Poem-specific, skill-based, developmentally-appropriate activities that 
+                        connect to the Common Core standards for poetry instruction accompany each poem.
+                        A K-5 TEKS version (Texas Essential Knowledge and Skills) is also available.
+                        <br><br>
+                        I’m honored to be one of the 75 poets who contributed original poems to this anthology.
+                        <br><br>
+                        To learn more, visit <a href="http://poetryfridayanthology.blogspot.com/">The Poetry Friday Anthology blog</a>.
+                        
+                    </p>
+                    <a target="_blank" href="http://www.amazon.com/Poetry-Friday-Anthology-Common-Core/dp/1937057682/ref=sr_1_1?s=books&ie=UTF8&qid=1437063216&sr=1-1&keywords=9781937057688">Buy on Amazon</a>
+                </div>
+                <br>
+                <br>
+<!-- nice on small screens up to 990, add class for large screens -->
+                <div class="pretty-divider col-md-12">
+                </div>
+            </div>
+
+
+<!-- Little Illinois -->
+            <div class="row book">
+                <img class="col-md-2 offset-one-fifth top-buffer " src="/img/book-illinois.jpg">
+                
+                <div class="col-md-6">
+                    <h2><em>Little Illinois</em>
+                    </h2>
+                    <p>
+                        By Esther Hershenhorn <br>
+                        Illustrated by Glenn Munroe<br>
+                        Sleeping Bear Press, 2011  <br>
+                        ISBN:  13:978-1-58536-537-1
+                    </p>
+                    <br>
+                    <p>
+                        In an easy-to-read board book format, "Little Illinois" shares ten rhyming 
+                        riddles to introduce young readers to Illinois’ state symbols and identifying features.
+                        <br>
+                        Young children will delight in the rhyming riddles, each framed by brightly-painted clues.
+                        <br>
+                        <br>
+                        "Little Illinois" is part of the Little State series from Sleeping Bear Press.
+                        
+                    </p>
+                    <a target="_blank" href="http://www.amazon.com/Little-Illinois-State-Esther-Hershenhorn/dp/1585365378/ref=sr_1_1?s=books&ie=UTF8&qid=1437063284&sr=1-1&keywords=little+illinois">Link to Amazon</a>
+                </div>
+                <br>
+                <br>
+<!-- nice on small screens up to 990, add class for large screens -->
+                <div class="pretty-divider col-md-12">
+                </div>
+            </div>
+
+<!-- S is for Story -->
+            <div class="row book">
+                <img class="col-md-2 offset-one-fifth top-buffer " src="/img/book-s-is-for-story.jpg">
+                
+                <div class="col-md-6">
+                    <h2><em>S is for Story: A Writer's Alphabet </em>
+                    </h2>
+                    <p>
+                        By Esther Hershenhorn <br>
+                        Illustrated by Zachary Pullen<br>
+                        Sleeping Bear Press, Date <br>
+                        ISBN: 978-58536-439-8
+                    </p>
+                    <br>
+                    <p>
+                        A true celebration of the Reader-Writer Connection, "S is for Story: A Writer’s Alphabet"
+                        offers readers an A-to-Z journey through a writer’s life and process. 
+                        <br>
+                        The iconic Sleeping Bear Press abecedarian format introduces in rhyme, 
+                        then amplifies in expository text selected essential aspects of becoming, being and living as a writer. 
+                        <br>
+                        Additional mini-Writing Lessons, Writer’s Tips, peeks at how writers work and treasured 
+                        authors’ helpful words ready and inspire readers to tell their good stories well. 
+                        <br>
+                        Spotlighted topics include idea generation, revision, editing, character, plot, word choice, 
+                        voice, journals, notebooks, letters and texting, fairy tales and genres, heroes and heroines.
+                        <br><br>
+                        I confess. "S is for Story: A Writer’s Alphabet" is the book I wish I’d owned as a little girl, 
+                    </p>
+                    <a target="_blank" href="http://www.amazon.com/Story-Writers-Alphabet-Books-Sleeping/dp/1585364398/ref=sr_1_1?ie=UTF8&qid=1437063475&sr=8-1&keywords=s+is+for+story">Buy on Amazon</a>
+                </div>
+                <br>
+                <br>
+<!-- nice on small screens up to 990, add class for large screens -->
+                <div class="pretty-divider col-md-12">
+                </div>
+            </div>
+
+<!-- Confessions -->
+            <div class="row book">
+                <img class="col-md-2 offset-one-fifth top-buffer " src="/img/book-confessions.jpg">
+                
+                <div class="col-md-6">
+                    <h2><em>The Confe$$ion$ and $ecret$ of Howard J. Fingerhut</em>
+                    </h2>
+                    <p>
+                        By Esther Hershenhorn <br>
+                        Holiday House, 2002 <br>
+                        ISBN: 0-8234-1642-9  
+                    </p>
+                    <br>
+                    <p>It was a no-brainer, at least in September. 
+                        <br>
+                        How could Howie not win the H. Marion Muckley Junior Businessperson of the Year Contest?  
+                        Didn't his lawn care business, A Boy for All Seasons, have $ucce$$ written all over it?  
+                        Why not chronicle his $ucce$$ September through June, then sell his journal as a how-to book for junior businesspeople! 
+                    </p>
+                    <p>
+                        How-to books offer advice, shortcuts and inspiration. 
+                        <br> 
+                        "The Confe$$ion$ and $ecret$ of Howard J. Fingerhut" offers that and so much more: 
+                        Humor, heart and a resilient spirit.
+                    </p>
+                    <a target="_blank" href="http://www.amazon.com/Confe-ion-ecret-Howard-Fingerhut/dp/0823416429/ref=sr_1_3?ie=UTF8&qid=1437064143&sr=8-3&keywords=esther+hershenhorn">Buy on  Amazon</a>
+                </div>
+                <br>
+                <br>
+<!-- nice on small screens up to 990, add class for large screens -->
+                <div class="pretty-divider col-md-12">
+                </div>
+            </div>
+
+
+<!-- Chicken Soup -->
+             <div class="row book">
+                <img class="col-md-2 offset-one-fifth top-buffer " src="/img/book-chicken-soup.jpg">
+                
+                <div class="col-md-6">
+                    <h2><em>Chicken Soup By Heart</em>
+                    </h2>
+                    <p>
+                        By Esther Hershenhorn<br>
+                        Illustrated by Rosanne Litzinger <br>
+                        Simon & Schuster <br>
+                        ISBN: 0-689-82665-6
+                    </p>
+                    <br>
+                    <p>
+                        Rudie Dinkins heard his mama say that Rudie's sitter, Mrs. Gittel, had the flu.
+                        Rudie needs his after-school babysitter Mrs. Gittel good-as-new by the next afternoon!
+                    </p>
+                    <p>
+                        Rudie decides he'll cook Mrs. Gittel chicken soup just the way she cooks it for him.  
+                        It just so happens he knows her Chicken Soup Secret: she stirs in very nice stories about her soon-to-be soup-eaters.
+                    </p>
+                    <p>
+                        Rudie stirs in such nice (and sweet) stories.  How could they not save the Chicken Soup Queen?
+                    </p>
+                    <a target="_blank" href="http://www.amazon.com/Chicken-Soup-Heart-Esther-Hershenhorn/dp/1442421975/ref=sr_1_1?ie=UTF8&qid=1437064758&sr=8-1&keywords=chicken+soup+by+heart">Buy on Amazon</a>
+                </div>
+                </div>
+                <br>
+                <br>
+<!-- nice on small screens up to 990, add class for large screens -->
+                <div class="pretty-divider col-md-12">
+                </div>
+            </div>
+        
+
+
+
+<!-- Fancy That -->
+            <div class="row book">
+                <img class="col-md-2 offset-one-fifth top-buffer " src="/img/book-fancy-that.jpg">
+                
+                <div class="col-md-6">
+                    <h2><em>Fancy That</em>
+                    </h2>
+                    <p>
+                        By Esther Hershenhorn<br>
+                        Illustrated by Megan Lloyd<br>
+                        Holiday House <br>
+                        ISBN: 0-8234-1603-4
+                    </p>
+                    <br>
+                    <p class="book-quote">
+                        <em>"Pippin Biddle did the only thing he could once he and his sisters were orphaned without warning.  He packed up his father's paints and readied for the road.</em>
+                    </p>
+                    <p class="book-quote">
+                        <em>“Be brave!” he told the sweet three.  There was Emma, the oldest, and Lyddie, the littlest, and Martha, in-between. “I'll travel about and paint people's portraits, earning my keep as Father once did.  I promise by Christmas we'll have our own home.”</em> 
+                    </p>
+                    <p>
+                        But the life of a limner was not as easy as Pip expected.  
+                    </p>
+                    <p>
+                        Spring turned to summer, summer turned to fall, and still Pip's paintings went unsold.  His young heart broke for his faraway sisters.
+                        Little did Pip know that his faraway sisters had plans of their own, thanks to Pip and his portraits.
+                    </p>
+                    <a target="_blank" href="http://www.amazon.com/Fancy-That-Esther-Hershenhorn/dp/0823416054/ref=sr_1_1?ie=UTF8&qid=1437065224&sr=8-1&keywords=fancy+that+esther+hershenhorn">Buy on Amazon</a>
+                </div>
+                </div>
+                <br>
+                <br>
+                <!-- nice on small screens up to 990, add class for large screens -->
+                <div class="pretty-divider col-md-12">
+                </div>
+            </div>
+        
+<!-- Lowell Party -->
+            <div class="row book">
+                <img class="col-md-2 offset-one-fifth top-buffer " src="/img/book-lowells-party.jpg">
+                
+                <div class="col-md-6">
+                    <h2><em>Lengthy Title</em>
+                    </h2>
+                    <p>
+                        Written by Esther Hershenhorn <br>
+                        Illustrated by Jacqueline Rogers<br>
+                        Holiday House, 1998 <br>
+                        ISBN: 0-8234-1313-6
+                    </p>
+                    <br>
+                    <p>
+                        Lowell visions a May Birthday Party with wall-to-wall kin: 
+                        Crumm Creek loggers from his Mama's side, Piggott's Peak barrel-makers from his Papa's side, 
+                        plus a passel of Granny Slocum's grape-growin', fiddlin' relatives from up on Slocum's Bluff.
+                    </p>
+                    <p>
+                        No matter that an Ozark rain storm is a-loomin' in the distance.  
+                        Lowell knows jest as sure as snakes crawl that even iffen red skies 
+                        an' low geese  an' leafbacks mean rain, and even iffen that rain begets floods, 
+                        slides an' twisters, no hard-workin', family-lovin', smart an' clever folk would 
+                        a-miss a kin's Birthday jest because of some pourdown.  
+                    </p>
+                    <p>
+                        No siree. They'd do what they had to.
+                    </p>
+                    <a target="_blank" href="http://www.amazon.com/There-Lowells-Party-Ester-Hershenhorn/dp/0823413136/ref=sr_1_1?ie=UTF8&qid=1437066257&sr=8-1&keywords=0823413136">Buy on Amazon</a>
+                </div>
+                <br>
+                <br>
+<!-- nice on small screens up to 990, add class for large screens -->
+                <div class="pretty-divider col-md-12">
+                </div>
+            </div>
+
+
+<!-- TO BE USED FOR THE LOOP IF I GET IT DONE -->
+
+<!--               <div class="book">
+                <img class="col-md-2 offset-one-fifth top-buffer " src="/img/book-poetry-friday.jpg">
+                
+                <div class="col-md-6">
+                    <h2><em>Lengthy Title</em>
+                    </h2>
+                    <p>
+                        Author <br>
+                        Publisher, Date <br>
+                        ISBN: 
+                    </p>
+                    <br>
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                         tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                        
+                    </p>
+                    <a href="">Buy on Amazon</a>
+                </div>
+                </div>
+                <br>
+                <br>
+
+                <div class="pretty-divider col-md-12">
+                </div>
+            </div>
+ -->
+        
+        
+
+         <!-- Contact Button -->
+        <?php include 'contact-button.html'; ?>
+        
+    </div>
+    
+    <!-- Footer -->
+    <?php include 'footer.php'; ?>
+
+	<script type="text/javascript" src="/js/jquery-2.1.4.min.js"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+
+</body>
+</html>
