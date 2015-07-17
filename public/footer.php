13e60b83f48761961c86a0f657944fc518ca25a8
Paula C Schuler <paulasatx@gmail.com>
2015-07-16 20:11:57 -0500

    Footer has its own file. Footer given a navigation menu for user convenience.

---
+++ public/footer.php
@@ -0,0 +1,9 @@
+<footer class="row">
+    <ul class="col-md-9">
+    	<li>&copy; 2015 Esther Hershenhorn</li>
+        <li><a href="index.php">Home</a></li>
+        <li><a href="about.php">About</a></li>
+        <li><a href="books.php">Books</a></li>
+        <li><a href="coaching.php">Coaching</a></li>
+    </ul>
+</footer>
