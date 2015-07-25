3517dcb5da8d6d3dc8e9aabde275ee77e5f2ffb9
Paula C Schuler <paulasatx@gmail.com>
2015-07-25 13:46:16 -0500

    Added my git and social links, codeup link and button.

--- public/footer.php
+++ public/footer.php
@@ -1,7 +1,14 @@
 <!-- WANT TODO:   -->
 
 <footer class="row">
-    <ul class="col-md-12 col-xs-12">
-    	<li>&copy; 2015 Esther Hershenhorn</li>
-    </ul>
+    <div id="by-line-paula" class="top-buffer-4 bottom-buffer-4">&copy; 2015 Designed by Paula C. Schuler as an educational portfolio website redesign of estherhershenhorn.com</div>
+    
+    <div class="bottom-buffer-4">
+		<a  id="codeup-button" href="http://codeup.com/" class="btn btn-lg button-contact my-button" role="button">Codeup<br>Alumni 2015 </a>
+	</div>
+    <div class="bottom-buffer-4">
+    	<a id="footer-linkedin-icon" href="https://www.linkedin.com/in/paulacschuler"><i class="contact-links icon icon-3x icon-linkedin-square"></i></a>
+    	<a id="footer-github-icon" href="https://github.com/paula-c-schuler"><i class="contact-links icon icon-3x icon-github"></i></a>
+    </div>	
+    
 </footer>
