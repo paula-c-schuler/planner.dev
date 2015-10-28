b64497419baac3e8ef5677bc570b8d63e5236f2f
Paula C Schuler <paulasatx@gmail.com>
2015-10-28 11:04:54 -0500

    Added nav tag, added display none to li tag in nav

--- public/navbar.php
+++ public/navbar.php
@@ -1,8 +1,10 @@
 <!-- Naviation bar for Esther Project -->
-    <ul id="navigation-top" class="row">
-        <li class="col-1-6 col-xs-6"><a href="index.php">Home</a></li>
-        <li class="col-1-6 col-xs-6"><a href="about.php">About</a></li>
-        <li class="col-1-6 col-xs-6"><a href="books.php">Books</a></li>
-        <li class="col-1-6 col-xs-6"><a href="coaching.php">Coaching</a></li>
-    </ul>
+    <nav>
+	    <ul id="navigation-top" class="row">
+	        <li class="col-1-6 col-xs-6"><a href="index.php">Home</a></li>
+	        <li class="col-1-6 col-xs-6"><a href="about.php">About</a></li>
+	        <li class="col-1-6 col-xs-6"><a href="books.php">Books</a></li>
+	        <li class="col-1-6 col-xs-6"><a href="coaching.php">Coaching</a></li>
+	    </ul>
+    </nav>
 
